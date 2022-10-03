ca465e8fc311d1d3dce3be5243a233e8f0de4810
Vaibhav-afk <vbhvka@gmail.com>
2022-10-03 21:04:51 +0530

    firebase config added

--- src/components/firebase-config.js
+++ src/components/firebase-config.js
@@ -4,13 +4,13 @@ import { getFirestore } from "@firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: "",
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId,
 };
 
 // Initialize Firebase
